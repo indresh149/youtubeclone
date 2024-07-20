811a10387ee4ae9a3940481b10a6ceff8d0829a1
indresh149 <indreshgoswami149@gmail.com>
2024-07-20 13:20:38 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
 
 
+  
 // import express from "express";
 // const app = express();
 

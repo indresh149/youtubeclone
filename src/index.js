286354197c6bfd94dde5ab7a6a4b9623e8112b9d
indresh149 <indreshgoswami149@gmail.com>
2024-07-27 21:55:49 +0530

    refresh token generate

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./.env" });
 
 connectDB()
   .then(() => {
